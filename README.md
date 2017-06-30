63dfec934309319fb070d99ee45e98f011428cd4
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-06-30 15:57:58 -0500

    finished basic and 4/6 extra

--- README.md
+++ README.md
@@ -24,14 +24,14 @@
 
 ## Extra Credit Requirements
 
-* Your calculator should be able to do more than one opertion per statement, i.e. 10 + 5 - 2.
+* Your calculator should be able to do more than one opertion per statement, i.e. 10 + 5 - 2. xx
 
-* Once your calculator completes an operation/statement, the next key entered should start a new operation.
+* Once your calculator completes an operation/statement, the next key entered should start a new operation. xx
 
-* Your calculator should make sure its first input is a number (save for the '-' symbol).
+* Your calculator should make sure its first input is a number (save for the '-' symbol). xx
 
 * All inputs/results that are numbers should be displayd in the readout, and the result should be calculated as you move to the next operation e.g. if your operations were 1 + 2 + 4, when you press the second '+' the readout should show 3.
 
-* Make the '.' operate correctly.
+* Make the '.' operate correctly.xx
 
 * Your calculator should evaluate this order of inputs correctly 5 + * 2 - 3 (the result should be 7, not 4, i.e. the '+' should be ignored).
--- main.js
+++ main.js
@@ -1,37 +1,75 @@
 var Numbers = []
+Equation=Numbers.join('')
 
 document.addEventListener("DOMContentLoaded", function(event){
 })
-
 document.addEventListener('click' , function(e) {
-
         console.log(e.srcElement)
         console.log(e.target.innerHTML)
         console.log(e.target.className);
-    if(e.target.className == 'button' ){
+        // if (Numbers.length -1 == '*' || '-' || '+' || '/' ){
+        //     Numbers.splice(Numbers.length -1,1)
+        // }
+        if(e.target.className == 'clear'){
+    Numbers=[];
+    Equation= [];
+    readout.innerHTML=null;
+}
+
+        if(e.target.className == 'button'  &&  Numbers.length ==  0){
+            console.log('first')
+            document.getElementById('readout')
+            readout.innerHTML=null
+            document.getElementById('readout').append(e.target.innerHTML)
+            Numbers.push(e.target.innerHTML)
+            e.target.style.background ='orange'
+    setTimeout(resetBackG, 50 )
+    function resetBackG(){
+    e.target.style.background = 'white'
+}
+        }
+    else if(e.target.className == 'button' ){
         console.log('made it')
     document.getElementById('readout').append(e.target.innerHTML)
-    e.target.style.background ='red'
+    Numbers.push(e.target.innerHTML )
+    e.target.style.background ='orange'
     setTimeout(resetBackG, 50 )
     function resetBackG(){
     e.target.style.background = 'white'
 }
 }
-else if(e.target.className == 'action'){
-e.target.style.background ='red'
+else if (e.target.id == 'subtract' && Numbers.length < 1){
+    console.log('subby')
+    document.getElementById('readout').append(e.target.innerHTML)
+    Numbers.push(e.target.innerHTML)
+}
+else if(e.target.className == 'action' && Numbers.length  && (Numbers.last()) !== ('-'||'+'||'*'||'/')){
+    console.log('ordinator')
+e.target.style.background ='orange'
     setTimeout(resetBackG, 50 )
     function resetBackG(){
     e.target.style.background = 'grey'
 }
-Numbers.push(readout.innerHTML )
 Numbers.push(e.target.innerHTML)
 readout.innerHTML= null
 }
-else( e.target.className == 'equals')
-// Numbers.push(readout.innerHTML)
-// Equation=Numbers.join( )
-    // console.log(Equation)
+else if(e.target.className == 'equals' && Numbers.length && (Numbers.last()) !== ('-'||'+'||'*'||'/')){
+Equation=Numbers.join('')
+    console.log(Equation)
+    console.log(eval(Equation))
+    readout.innerHTML=null
+    readout.innerHTML=eval(Equation)
+    Equation=null
+    console.log(Numbers)
+    console.log(Equation)
+    Numbers=[]
+}
 });
 
 
 
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
